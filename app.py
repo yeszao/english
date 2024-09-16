6dab43d16d539e9efa4e3d428d9c544c282c2aa0
Meng Guozao <meng.guozao@core42.ai>
2024-09-16 16:07:11 +0400

    first useful version

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from pathlib import Path
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+books_dir = Path('books')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+
+    return render_template('home.html')
+
+
+@app.route('/<book_name>/<chapter_file>')
+def chapter(book_name: str, chapter_file: str):
+    content = books_dir.joinpath(book_name).joinpath(chapter_file).read_text()
+    return render_template('chapter.html', content=content)
 
 
 if __name__ == '__main__':
