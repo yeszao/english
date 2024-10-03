7efbb102ee15c2ae1a12ec6055ebb40660222401
Guozao Meng <guozao.m@gmail.com>
2024-10-03 11:44:44 +0400

    Split by word

--- src/utils/chapter_utils.py
+++ src/utils/chapter_utils.py
@@ -50,6 +50,23 @@ def wrap_words(text, start_tag='<span class="word">', end_tag='</span>'):
     doc = get_words(text)
     wrapped = ''
 
+    for token in doc:
+        if token.is_alpha:
+            wrapped += f'{start_tag}{token.text}{end_tag}'
+        else:
+            # Keep punctuation and non-alpha tokens unchanged
+            wrapped += token.text
+
+        if token.whitespace_:
+            wrapped += token.whitespace_
+
+    return wrapped
+
+
+def wrap_words_and_entities(text, start_tag='<span class="word">', end_tag='</span>'):
+    doc = get_words(text)
+    wrapped = ''
+
     # Merge entities (such as "New York" or "John Smith") into single tokens
     with doc.retokenize() as retokenizer:
         for ent in doc.ents:
--- tests/test_chapter_utils.py
+++ tests/test_chapter_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.utils.chapter_utils import tagged_html, wrap_words
+from src.utils.chapter_utils import tagged_html, wrap_words, wrap_words_and_entities
 from tests import TEST_FILES_DIR
 
 input_file = TEST_FILES_DIR.joinpath("chapter_utils").joinpath('chapter.html')
@@ -16,7 +16,17 @@ def test_tagged_html():
 
 @pytest.mark.parametrize("text, expected", [
     ("Hello, world!", '[Hello], [world]!'),
-    ("John Smith was born in New York but moved to Los Angeles in United States.", '[John Smith] [was] [born] [in] [New York] [but] [moved] [to] [Los Angeles] [in] [United States].'),
+    ("I love Perter's laptop.", '[I] [love] [Perter]\'s [laptop].'),
+    ("John Smith was born in New York but moved to Los Angeles in United States.", '[John] [Smith] [was] [born] [in] [New] [York] [but] [moved] [to] [Los] [Angeles] [in] [United] [States].'),
 ])
 def test_wrap_words(text, expected):
     assert wrap_words(text, '[', ']') == expected
+
+
+@pytest.mark.parametrize("text, expected", [
+    ("Hello, world!", '[Hello], [world]!'),
+    ("I love Perter's laptop.", '[I] [love] [Perter]\'s [laptop].'),
+    ("John Smith was born in New York but moved to Los Angeles in United States.", '[John Smith] [was] [born] [in] [New York] [but] [moved] [to] [Los Angeles] [in] [United States].'),
+])
+def test_wrap_words_and_entities(text, expected):
+    assert wrap_words_and_entities(text, '[', ']') == expected
