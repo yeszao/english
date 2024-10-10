debb1a17863768379eeb3ea87725d0084d80a9a0
Meng Guozao <meng.guozao@core42.ai>
2024-10-10 09:16:21 +0400

    Download en_core_web_sm

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,6 +1,3 @@
 !/bin/bash
 
-docker exec -it english-web python /workspace/src/crawl_news.py
-
-
 docker run --rm --env-file /home/gary/docker_data/english/local.env english-web python /workspace/src/crawl_news.py
